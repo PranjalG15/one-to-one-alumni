b7295d5ef904aa1f93e746e0ca054569cb0cc2f8
PranjalG15 <aryapranjal1999@gmail.com>
2021-06-04 11:58:05 +0530

    new change

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,25 @@
 var socket = io();
-// listen for server connection
-// get query params from url
 var name = getQueryVariable("name") || 'Anonymous';
 var room = getQueryVariable("room") || 'No Room Selected';
 
 $(".room-title").text(room);
-// fires when client successfully conencts to the server
 socket.on("connect", function() {
   console.log("Connected to Socket I/O Server!");
   console.log(name + " wants to join  " + room);
-  // to join a specific room
   socket.emit('joinRoom', {
     name: name,
     room: room
   });
 });
 
-// below code is to know when typing is there
 var timeout;
 
 function timeoutFunction() {
   typing = false;
-  //console.log("stopped typing");
-  // socket.emit("typing", false);
   socket.emit('typing', {
     text: "" //name + " stopped typing"
   });
 }
-// if key is pressed typing message is seen else auto after 2 sec typing false message is send
-// TODO : add broadcast event when server receives typing event
 $('#messagebox').keyup(function() {
   console.log('happening');
   typing = true;
@@ -92,12 +83,9 @@ socket.on("message", function(message) {
   var $message = $('<li class = "list-group-item"></li>');
 
   var momentTimestamp = moment.utc(message.timestamp).local().format("h:mm a");
-  //$(".messages").append($('<p>').text(message.text));
   $message.append("<strong>" + momentTimestamp + " " + message.name + "</strong>");
   $message.append("<p>" + message.text + "</p>");
   $messages.append($message);
-  // handle autoscroll
-  // manage autoscroll
   var obj = $("ul.messages.list-group");
   var offset = obj.offset();
   var scrollLength = obj[0].scrollHeight;
@@ -106,17 +94,14 @@ socket.on("message", function(message) {
     scrollTop: scrollLength - offset.top
   });
 
-  // try notify , only when user has not open chat view
   if (document[hidden]) {
     notifyMe(message);
-    // also notify server that user has not seen messgae
     var umsg = {
       text: name + " has not seen message",
       read: false
     };
     socket.emit("userSeen", umsg);
   } else {
-    // notify  server that user has seen message
     var umsg = {
       text: name + " has seen message",
       read: true,
@@ -126,15 +111,13 @@ socket.on("message", function(message) {
   }
 });
 
-// handles submitting of new message
 var $form = $("#messageForm");
 var $message1 = $form.find('input[name=message]');
 $form.on("submit", function(event) {
   event.preventDefault();
   var msg = $message1.val();
-  //prevent js injection attack
   msg = msg.replace(/</g, "&lt;").replace(/>/g, "&gt;").trim();
-  if (msg === "") return -1; //empty messages cannot be sent
+  if (msg === "") return -1; 
 
   socket.emit("message", {
     text: msg,
@@ -145,9 +128,7 @@ $form.on("submit", function(event) {
   var $message = $('<li class = "list-group-item"></li>');
 
   var momentTimestamp = moment().format("h:mm a");
-  // $(".messages").append($('<p>').text(message.text));
   $message.append("<strong>" + momentTimestamp + " " + name + "</strong>");
-  //$message.append("<p>" + $message1.val()+ "</p>");
   $message.append($("<p>", {
     class: "mymessages",
     text: $message1.val()
@@ -165,42 +146,33 @@ $form.on("submit", function(event) {
 
 });
 
-// notification message
 function notifyMe(msg) {
-  // Let's check if the browser supports notifications
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification,try Chromium!");
   }
 
-  // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    //  var notification = new Notification(msg);
     var notification = new Notification('Chat App', {
       body: msg.name + ": " + msg.text,
-      icon: '/images/apple-icon.png' // optional
+      icon: '/images/apple-icon.png'
     });
     notification.onclick = function(event) {
       event.preventDefault();
       this.close();
-      // assume user would see message so broadcast userSeen event
       var umsg = {
         text: name + " has seen message",
         read: true,
         user: name
       };
       socket.emit("userSeen", umsg);
-      //window.open('http://www.mozilla.org', '_blank');
     };
   }
-  // Otherwise, we need to ask the user for permission
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function(permission) {
-      // If the user accepts, let's create a notification
       if (permission === "granted") {
         var notification = new Notification('Chat App', {
           body: msg.name + ": " + msg.text,
-          icon: '/images/apple-icon.png' // optional
+      
         });
         notification.onclick = function(event) {
           event.preventDefault();
@@ -211,13 +183,8 @@ function notifyMe(msg) {
             user: name
           };
           socket.emit("userSeen", umsg);
-          // assume user would see message so broadcast userSeen event
-          //window.open('http://www.mozilla.org', '_blank');
-        };
+           };
       }
     });
   }
-
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
 }
